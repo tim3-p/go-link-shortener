c4c6801e9199790d8478bbccd01a6f7b10e58cfc
Timofey Pavlov <timofey.p@vendoo.uz>
2022-02-26 15:00:00 +0500

    fix: fixed autotests

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -6,8 +6,8 @@ const (
 )
 */
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
 var EnvConfig Config
