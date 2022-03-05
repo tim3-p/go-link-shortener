6ff072d2fd0a96f41d8769c77e881bfb2e4089c8
Timofey Pavlov <timofey.p@vendoo.uz>
2022-03-05 15:24:07 +0500

    fix: changed compress logic

--- internal/app/compress.go
+++ internal/app/compress.go
@@ -14,6 +14,15 @@ type gzipWriter struct {
 
 func GzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer gz.Close()
+			r.Body = gz
+		}
 
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
