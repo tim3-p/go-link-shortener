ffd5e2a132d095973d5229b7689fa5eb7893de7b
Timofey Pavlov <timofey.p@vendoo.uz>
2022-03-12 10:22:58 +0500

    fix: changed userID logic

--- internal/storage/map.go
+++ internal/storage/map.go
@@ -6,40 +6,26 @@ import (
 
 type MapRepository struct {
 	urlBase   map[string]string
-	userLinks map[string][]string
+	userLinks map[string]string
 }
 
 func NewMapRepository() *MapRepository {
-	return &MapRepository{urlBase: make(map[string]string), userLinks: make(map[string][]string)}
+	return &MapRepository{urlBase: make(map[string]string)}
 }
 
 func (r *MapRepository) Add(key, value, userID string) error {
 	r.urlBase[key] = value
-	r.userLinks[userID] = append(r.userLinks[userID], key)
 	return nil
 }
 
 func (r *MapRepository) Get(key, userID string) (string, error) {
-	userMap := r.userLinks[userID]
-
-	for _, arrValue := range userMap {
-		if value, ok := r.urlBase[arrValue]; ok {
-			return value, nil
-		}
+	if value, ok := r.urlBase[key]; ok {
+		return value, nil
+	} else {
+		return "", errors.New("key not found")
 	}
-
-	return "", errors.New("key not found")
 }
 
 func (r *MapRepository) GetUserURLs(userID string) (map[string]string, error) {
-	userMap := r.userLinks[userID]
-	result := make(map[string]string)
-
-	for _, arrValue := range userMap {
-		if value, ok := r.urlBase[arrValue]; ok {
-			result[arrValue] = value
-		}
-	}
-
-	return result, nil
+	return r.urlBase, nil
 }
