816a909e4af8945b18613281e572c8e4ee533399
Timofey Pavlov <timofey.p@vendoo.uz>
2022-03-12 16:03:53 +0500

    feat: changed userID logic

--- internal/storage/map.go
+++ internal/storage/map.go
@@ -16,7 +16,6 @@ func NewMapRepository() *MapRepository {
 
 func (r *MapRepository) Add(key, value, userID string) error {
 	log.Printf("Add userID - %s", userID)
-
 	log.Printf("Add userLinks before - %s", r.userLinks)
 	r.urlBase[key] = value
 	r.userLinks[userID] = append(r.userLinks[userID], key)
@@ -25,20 +24,27 @@ func (r *MapRepository) Add(key, value, userID string) error {
 }
 
 func (r *MapRepository) Get(key, userID string) (string, error) {
-	log.Printf("Get userID - %s", userID)
-	userMap := r.userLinks[userID]
-	log.Printf("Get userLinks - %s", r.userLinks)
-	log.Printf("Get userMap - %s", userMap)
+	/*
+		log.Printf("Get userID - %s", userID)
+		userMap := r.userLinks[userID]
+		log.Printf("Get userLinks - %s", r.userLinks)
+		log.Printf("Get userMap - %s", userMap)
 
-	log.Printf("Get key - %s", key)
+		log.Printf("Get key - %s", key)
 
-	for _, arrValue := range userMap {
-		if arrValue == key {
-			return r.urlBase[arrValue], nil
+		for _, arrValue := range userMap {
+			if arrValue == key {
+				return r.urlBase[arrValue], nil
+			}
 		}
-	}
 
-	return "", errors.New("key not found")
+		return "", errors.New("key not found")
+	*/
+	if value, ok := r.urlBase[key]; ok {
+		return value, nil
+	} else {
+		return "", errors.New("key not found")
+	}
 }
 
 func (r *MapRepository) GetUserURLs(userID string) (map[string]string, error) {
